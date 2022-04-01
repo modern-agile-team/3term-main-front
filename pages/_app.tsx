87c4db127cf6777179eb0a7e8666959444732e73
leephoter <estarg51@gmail.com>
2022-04-01 14:01:15 +0900

    Add/hangyeol:added border radius styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,5 @@
 import type { AppProps } from 'next/app';
 import { injectGlobal, css, cx } from '@emotion/css';
-import styled from '@emotion/styled';
-import { colors, color } from '../style/palette';
-import Button from '../components/button/Button';
 
 injectGlobal`
 * {
@@ -19,10 +16,6 @@ injectGlobal`
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
-
-  // 2. 배경 넘기고, 텍스트 넘긴다
-  // color.back(색깔)
-  // color.content(색깔)
 }
 
 export default MyApp;
